30f2cdcff1b9a5af78fbe8a86af1f54b2bffe4da
lucifer2355 <dgajjar999@gmail.com>
2021-09-27 19:41:54 +0530

    create strudent controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { StudentController } from '../controller/student/strudent.controller';
 
 @Module({
   imports: [],
+  controllers: [StudentController],
 })
 export class AppModule {}
